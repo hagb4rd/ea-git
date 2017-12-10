16c46db8a79fbd1e19d7158d1784159eadec8dd7
hagb4rd <hagb4rd@gmail.com>
2017-12-10 10:26:53 +0100

    Gist Class added.

--- index.js
+++ index.js
@@ -1,46 +1,13 @@
-var url = require("url");
-var util = require("util");
+//Catch uncaught errors
+var util=require("util");
 
+process.on('uncaught', function(e) {
+	console.log(util.inspect(e.stack,{showHidden: false, depth:null, color: true}));
+});
 
-
-function GitUrl() {
-	
-	var git = this; 
-	git.usr = "";
-	git.prj = ""; 
-	git.branch = "";
-	git.tree = ""; 
-	git.file = ""; 
-	git.fileext = "";
+module.exports = {
+	credentials: require("./lib/credentials"),
+	gist: require("./lib/gist.paste"),
+	GitUrl: require("./lib/giturl"),
+	Gist: require("./lib/gist")
 }
-GitUrl.prototype.toRaw = function() {
-	
-	var urlObject = {
-		protocol = 'https:',
-		host = "raw.githubusercontent.com",
-		slashes = true,
-		pathname = "/" + ([this.usr, this.prj, this.branch].concat(this.tree)).join("/")
-	};
-	return url.format(urlObject);
-};
-
-
-GitUrl.fromRaw = (rawUrl) => {	
-	// "https://raw.githubusercontent.com/zalun/jsFiddleGithubDemo/master/Demo/demo.response.html"
-	var uri = url.parse(rawUrl); 
-	var pathArr = uri.pathname.split("/").slice(1); // [ 'zalun', 'jsFiddleGithubDemo', 'master', 'Demo', 'demo.response.html' ] 
-
-	var git = new GirUrl();
-	git.usr = pathArr.shift()  // 'zalun'
-	git.prj = pathArr.shift(); // 'jsFiddleGithubDemo'
-	git.branch = pathArr.shift() // 'master'
-	git.tree = pathArr.slice(0); //[ 'Demo', 'demo.response.html' ] 	
-	git.file = pathArr[pathArr.length-1];
-	git.fileext = "";
-	var extIndex = git.file.lastIndexOf(".");
-	if(extIndex >= 0) {
-		git.fileext = git.file.slice(extIndex);
-	}
-}
-
-module.exports = GitUrl; 
